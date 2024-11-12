dcc271f55b0cf274f5ced9924070d4fc991d0ebe
joelthiek <joelrokiemlothiek@gmail.com>
2024-11-12 17:13:30 +0530

    setting up folder structure and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
-import './App.css'
-
+import {Routes,Route} from "react-router-dom"
+import LandingPage from "./pages/LandingPage/LandingPage"
+import SignInPage from "./pages/SignInPage/SignInPage"
+import SignUpPage from "./pages/SignUpPage/SignUpPage"
 function App() {
 
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/auth" element={<SignInPage/>}/>
+        <Route path="/register" element={<SignUpPage/>}/>
+      </Routes>
     </div>
   )
 }
