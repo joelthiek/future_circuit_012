c2feacecea9acca5e32b4027b58996b12bcff0b3
Himanshu Ladekar <himanshuladekar21@gmail.com>
2024-11-16 15:40:46 +0530

    updated condition for rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,28 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
-import { BrowserRouter as Router } from "react-router-dom"
-import Footer from "./components/Footer/Footer.jsx"
+import Footer from './components/Footer/Footer.jsx';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+
+function Layout() {
+  const location = useLocation();
+
+  // Define the paths where the footer should not appear
+  const hideFooterPaths = ['/create-resume'];
+
+  return (
+    <>
+      <Navbar />
+      <App />
+      {/* Conditionally render Footer */}
+      {!hideFooterPaths.includes(location.pathname) && <Footer />}
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <Router>
-    <Navbar />
-    <App />
-    <Footer/>
-  </Router>,
-)
+    <Layout />
+  </Router>
+);
